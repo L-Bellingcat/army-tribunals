470f24822122040fed4e1dfc9a366e4da7f5429b
L Bellingcat <l.bellingcat.hackathon>
2023-04-22 12:01:35 +0100

    cosine search working

--- chandigarh_embed.py
+++ chandigarh_embed.py
@@ -1,41 +1,12 @@
-import os
-
-import openai
 import pandas as pd
 import tiktoken
-from dotenv import load_dotenv
 from pdfminer.high_level import extract_text
 from tqdm import tqdm
 
+from embed import embed_text
+
 enc = tiktoken.get_encoding("cl100k_base")
 assert enc.decode(enc.encode("hello world")) == "hello world"
-
-# To get the tokeniser corresponding to a specific model in the OpenAI API:
-enc = tiktoken.encoding_for_model("gpt-4")
-
-load_dotenv()
-openai.organization = os.getenv("OPENAI_ORG_ID")
-openai.api_key = os.getenv("OPENAI_API_KEY")
-
-MAX_TOKENS = 8191
-ENCODING = tiktoken.get_encoding("cl100k_base")
-
-
-def trim_text(text: str, max_tokens: int = MAX_TOKENS):
-    tokens = ENCODING.encode(text)
-    if len(tokens) > max_tokens:
-        tokens = tokens[:max_tokens]
-    return ENCODING.decode(tokens)
-
-
-def embed_text(text: str):
-    text = trim_text(text)
-
-    response = openai.Embedding.create(
-        input=text,
-        model="text-embedding-ada-002"
-    )
-    return response['data'][0]['embedding']
 
 
 def process_row(row: pd.Series) -> pd.Series:
--- cosine_search.py
+++ cosine_search.py
@@ -1,8 +1,13 @@
+import time
+
 import numpy as np
 import pandas as pd
+from pandas import DataFrame
+
+from embed import embed_text
 
 
-def main():
+def create_index():
     data = pd.read_csv('embeddings/chandigarh.csv')
 
     # Remove all rows that have NaN values
@@ -24,5 +29,55 @@ def main():
     similarities.to_csv('similarities/chandigarh.csv', index=False)
 
 
+class FindSimilar:
+    def __init__(self, df: DataFrame):
+        self.df = df
+
+    def find_closest(self, text: str, K: int = 10):
+        """Embed the text and find the closest judgement in the index using cosine similarity."""
+        embedding = embed_text(text)
+
+        # Compute the cosine similarity between the embedding and all the embeddings in the index
+        self.df['similarity'] = self.df['embedding'].apply(
+            lambda x: np.dot(x, embedding) / (np.linalg.norm(x) * np.linalg.norm(embedding)))
+
+        # Sort the df by similarity in descending order
+        similarities = self.df.sort_values(by='similarity', ascending=False)
+
+        # Return the top K similar judgements
+        return similarities.head(K)
+
+
+def load_data():
+    data = pd.read_csv('embeddings/chandigarh.csv')
+
+    # Remove all rows that have NaN values
+    data = data.dropna()
+
+    # Convert the 'embedding' column from string to numpy array
+    data['embedding'] = data['embedding'].apply(
+        lambda x: np.fromstring(x[1:-1], sep=',')
+    )
+
+    return data
+
+
+def main():
+    # Create an instance of FindSimilar
+
+    start = time.time()
+    find_similar = FindSimilar(load_data())
+    end = time.time()
+    print("Time taken to load data: ", end - start)
+
+    # Find the closest judgement to the given text
+    print(find_similar.find_closest(
+        "Bhani Devi Vs. Union of India & Ors"
+    ))
+
+
 if __name__ == '__main__':
-    main()
+    # time main() execution time
+    import timeit
+
+    print(timeit.timeit(main, number=1))
