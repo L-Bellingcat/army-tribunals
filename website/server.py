94501f5236d64eca9ca4db570438c8f1915aef0f
L Bellingcat <l.bellingcat.hackathon>
2023-04-23 12:21:46 +0100

    server working with summarisation

--- website/server.py
+++ website/server.py
@@ -2,6 +2,7 @@
 import os
 import shutil
 import uuid
+from concurrent.futures import ThreadPoolExecutor
 
 import dotenv
 import uvicorn
@@ -13,6 +14,7 @@ from starlette.staticfiles import StaticFiles
 from starlette.templating import Jinja2Templates
 
 from cosine_search import load_chandigarh_df, FindSimilar
+from gpt3_parsing import summarise
 
 dotenv.load_dotenv()
 
@@ -42,8 +44,8 @@ async def home(request: Request):
 
 
 # Search for documents similar to a provided PDF
-@app.post("/search/")
-async def create_file(pdf: UploadFile):  # -> RedirectResponse:
+@app.post("/search/", response_class=HTMLResponse)
+async def create_file(request: Request, pdf: UploadFile):  # -> RedirectResponse:
     # Write to disk with a randomly generated filename
     filename = f"{uuid.uuid4()}.pdf"
     with open(filename, "wb") as buffer:
@@ -61,10 +63,21 @@ async def create_file(pdf: UploadFile):  # -> RedirectResponse:
     # Find the closest judgement to the given text
     similar = find_similar.find_closest(pdf_text, K=10)
 
-    # Remove the 'embedding', 'similarity' and 'pdf_text' columns
-    similar = similar.drop(columns=['embedding', 'similarity', 'pdf_text'])
+    # For each similar judgement summarise the PDF text and add it to the dataframe using LegalTextParser, do this in parallel then wait for all threads to complete
+    with ThreadPoolExecutor() as executor:
+        futures = [executor.submit(summarise, row['pdf_text']) for _, row in similar.iterrows()]
+        for future, (i, row) in zip(futures, similar.iterrows()):
+            # Save result into the similar dataframe
+            similar.loc[i, 'summary'] = future.result()
 
-    return similar.to_dict(orient='records')
+    # Remove the 'embedding' and 'pdf_text' columns
+    similar = similar.drop(columns=['embedding', 'pdf_text'])
+
+    # return templates.TemplateResponse("results.html", similar.to_dict(orient='records'))
+    return templates.TemplateResponse("results.html", context={
+        'request': request,
+        'results': similar.to_dict(orient='records')
+    })
 
 
 if __name__ == '__main__':
