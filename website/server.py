11cf1ba973dac77df7ca894225fa4030e4cc1824
Laurence Cullen <laurence@vanellus.tech>
2023-08-27 17:21:08 +0100

    adding plain text search functionality

--- website/server.py
+++ website/server.py
@@ -3,11 +3,13 @@ import os
 import shutil
 import uuid
 from concurrent.futures import ThreadPoolExecutor
+from typing import Annotated
 
 import dotenv
 import uvicorn
-from fastapi import FastAPI, UploadFile
+from fastapi import FastAPI, Form
 from fastapi import Request
+from fastapi import UploadFile
 from pdfminer.high_level import extract_text
 from starlette.responses import HTMLResponse
 from starlette.staticfiles import StaticFiles
@@ -44,8 +46,29 @@ async def home(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
 
 
+def similar_to_text(text: str):
+    # Find other judgements with similar PDF text
+    find_similar = FindSimilar(chandigarh_df)
+
+    # Find the closest judgement to the given text
+    similar = find_similar.find_closest(text, K=10)
+
+    # For each similar judgement summarise the PDF text and add it to the dataframe using LegalTextParser,
+    # do this in parallel then wait for all threads to complete
+    with ThreadPoolExecutor() as executor:
+        futures = [executor.submit(summarise, row['pdf_text']) for _, row in similar.iterrows()]
+        for future, (i, row) in zip(futures, similar.iterrows()):
+            # Save result into the similar dataframe
+            similar.loc[i, 'summary'] = future.result()
+
+    # Remove the 'embedding' and 'pdf_text' columns
+    similar = similar.drop(columns=['embedding', 'pdf_text'])
+
+    return similar
+
+
 # Search for documents similar to a provided PDF
-@app.post("/search", response_class=HTMLResponse)
+@app.post("/similar_cases_file", response_class=HTMLResponse)
 async def create_file(request: Request, pdf: UploadFile):  # -> RedirectResponse:
     # Write to disk with a randomly generated filename
     filename = f"{uuid.uuid4()}.pdf"
@@ -58,21 +81,18 @@ async def create_file(request: Request, pdf: UploadFile):  # -> RedirectResponse
     # Delete the file
     os.remove(filename)
 
-    # Find other judgements with similar PDF text
-    find_similar = FindSimilar(chandigarh_df)
+    similar = similar_to_text(pdf_text)
+    # return templates.TemplateResponse("results.html", similar.to_dict(orient='records'))
+    return templates.TemplateResponse("results.html", context={
+        'request': request,
+        'results': similar.to_dict(orient='records')
+    })
 
-    # Find the closest judgement to the given text
-    similar = find_similar.find_closest(pdf_text, K=10)
 
-    # For each similar judgement summarise the PDF text and add it to the dataframe using LegalTextParser, do this in parallel then wait for all threads to complete
-    with ThreadPoolExecutor() as executor:
-        futures = [executor.submit(summarise, row['pdf_text']) for _, row in similar.iterrows()]
-        for future, (i, row) in zip(futures, similar.iterrows()):
-            # Save result into the similar dataframe
-            similar.loc[i, 'summary'] = future.result()
-
-    # Remove the 'embedding' and 'pdf_text' columns
-    similar = similar.drop(columns=['embedding', 'pdf_text'])
+# Search for documents with a similar embedding to the embedding of passed in plain text search query
+@app.post("/similar_cases_text", response_class=HTMLResponse)
+async def cases_similar_to_text(request: Request, text: Annotated[str, Form()]):  # -> RedirectResponse:
+    similar = similar_to_text(text)
 
     # return templates.TemplateResponse("results.html", similar.to_dict(orient='records'))
     return templates.TemplateResponse("results.html", context={
